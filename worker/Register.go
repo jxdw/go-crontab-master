09d63edae98a1943b3c92786cd950193b7d9dbfb
liuxiaogang2 <chinaliuxiaogang@gmail.com>
2019-02-12 23:14:26 +0800

    修复获取ip的bug

--- worker/Register.go
+++ worker/Register.go
@@ -1,6 +1,7 @@
 package worker
 
 import (
+	"fmt"
 	"go.etcd.io/etcd/clientv3"
 	"net"
 	"go-crontab-master/common"
@@ -23,29 +24,50 @@ var (
 
 //获取本机网卡IP
 func getLocalIP() (ipv4 string, err error)  {
-	var (
-		addrs []net.Addr
-		addr net.Addr
-		ipNet *net.IPNet //IP地址
-		isIpNet bool
-	)
+	//var (
+	//	addrs []net.Addr
+	//	addr net.Addr
+	//	ipNet *net.IPNet //IP地址
+	//	isIpNet bool
+	//)
 
-	//获取所有网卡
-	if addrs, err = net.InterfaceAddrs(); err != nil{
-		return
+	netInterfaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Println("net.Interfaces failed, err:", err.Error())
 	}
 
-	//取第一个非lo的网卡IP
-	for _, addr = range addrs{
-		//这个网络地址是IP地址: ipv4,ipv6
-		if ipNet, isIpNet = addr.(*net.IPNet); isIpNet && !ipNet.IP.IsLoopback(){
-			//跳过IPV6
-			if ipNet.IP.To4() != nil{
-				ipv4 = ipNet.IP.String() //192.168.1.1
-				return
+	for i := 0; i < len(netInterfaces); i++ {
+		if (netInterfaces[i].Flags & net.FlagUp) != 0 {
+			addrs, _ := netInterfaces[i].Addrs()
+
+			for _, address := range addrs {
+				if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+					if ipnet.IP.To4() != nil {
+						ipv4=ipnet.IP.String()
+						return
+					}
+				}
 			}
 		}
 	}
+
+
+	////获取所有网卡
+	//if addrs, err = net.InterfaceAddrs(); err != nil{
+	//	return
+	//}
+	//
+	////取第一个非lo的网卡IP
+	//for _, addr = range addrs{
+	//	//这个网络地址是IP地址: ipv4,ipv6
+	//	if ipNet, isIpNet = addr.(*net.IPNet); isIpNet && !ipNet.IP.IsLoopback(){
+	//		//跳过IPV6
+	//		if ipNet.IP.To4() != nil{
+	//			ipv4 = ipNet.IP.String() //192.168.1.1
+	//			return
+	//		}
+	//	}
+	//}
 
 	err = common.ERR_NO_LOCAL_IP_FOUND
 	return
